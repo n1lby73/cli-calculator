f7e30ab437ea8e36cf29a307b9e40a8fd29a41c4
n1lby73 <65239245+n1lby73@users.noreply.github.com>
2022-12-26 10:17:57 +0100

    updated to the latest release

--- cli-calculator.py
+++ cli-calculator.py
@@ -1,180 +1,88 @@
-#simple cli calculator using python 
-#version ==> 1.0
-print ("Simple calculator using python\nVersion 1.0\n\nOpearions allowed are:\n\n1. Addition = +\n2. Subtraction = -\n3. Multiplication = *\n4. Division = /\n5. Power = ^\n6. Binary = b\n7. Percentage = %")
+"""Simple cli calculator using python 
+Version ==> 2.1
+The update fixed some stuff:
+	Added Quadratic operation """
 
-print (" ")
-#info from developer
-print("NOTE: if you're looking for:\n1. Binary value of a single number\n2. percentage value of a single number \n\nkindly enter:\n'0' when the prompt ask for your second value \n'+' when it ask for your binary/percentage operation ")
+#banner
 
-print (" ")
-#user's input
-variable_1 = int(input("your first value: "))
+print ("  ____________________---------------------------_____________________ \n |								      |\n | 		SIMPLE CALCULATOR USING PYTHON {Version 2.0}	      |\n |								      |\n | Opearions allowed are:   	  				      |\n |								      |\n |  (Addition = +) •••• (Subtraction = -) •••• (Multiplication = *)   |\n |								      |\n |       (Division = /) •••••• (Power = ^) •••••• (Binary = b)        |\n |								      |\n | 		(Percentage = %) •••• (Quadratic = q)			      |\n |								      |\n |  Note: If you're looking for the binary value of a single number   |\n |	input '0' as your second value and '+' as your binary	      |\n |				operation			      |\n --------------------______________________________--------------------\n\n")
 
-variable_2 = int(input('your second value: '))
+#listing all function
 
-operation = input("your operation: ")
+#user input ==> main function
 
-#Addition operatiion
+def user_input ():
+	
+	digit1 = int(input(" your first value: "))
+	
+	print (" ")
+	
+	sign = input(" your operation: ")
+	
+	print (" ")
+	
+	digit2 = int(input(" your second value: "))
+	
+	print (" ")
+	
+#result function
 
-if operation == "+":
-    print (" ")
-    print ("Your answer is: ", variable_1 + variable_2)
-    print (" ")
-    print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
+	def calc():
+		print (" Answer ==>",all_operation(digit1,digit2,digit3))
+		print(" ")
+		print(" ——————————————————————••••••••••••••••••••••••——————————————————————\n")
+		return user_input()
+		
+#binary operation function
 
-#Subtraction operation
+	def binary_operation(b,o):
+		ba = input(" your binary operation: ")
+		print (" ")
+		if ba == "+":
+			return(format((b+o), "b"))
+		elif ba == "-":
+			return(format((b-o), "b"))
+		elif ba == "*":
+			return (format((b*o),"b"))
+		elif ba == "/":
+			return (format(int((b/o)),"b"))
+		elif ba == "^":
+			return(format(pow(b,o),"b"))
+		else:
+			return "SYNTAX ERROR !!!"
 
-elif operation == "-":
-    print (" ")
-    print ("Your answer is: ", variable_1 - variable_2)
-    print (" ")
-    print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
+#quadratic operation function
 
-#Multiplication operation 
+    def quadratic_operation(a,b,c):
 
-elif operation == "*":
-    print (" ")
-    print ("Your answer is: ", variable_1 * variable_2 )
-    print (" ")
-    print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
+        return()
 
-#Division operation 
-#output changed from float to intgers due to int 
-#removed int cause it gives output like 0.5 as 0
+#operation function
+			
+	def all_operation(x,y,z=0):
+		if sign == "+":
+			return (x+y)
+		elif sign == "-":
+			return (x-y)
+		elif  sign == "*":
+			return (x*y)
+		elif sign == "/":
+			return (x/y)
+		elif sign == "%":
+			return ((x*y)/100)
+		elif sign == "^":
+			return pow(x,y)
+		elif sign == "b":
+			return binary_operation(x,y)
+        elif sign == "q":
+            digit3 = int(input(" your third value: "))
+            return quadratic_operation(x,y,z)
+		else:
+			return "SYNTAX ERROR !!!"
+			
+	return (calc())
 
-elif operation == "/":
-    print (" ")
-    print ("Your answer is: ", variable_1 / variable_2)
-    print (" ")
-    print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
+#equation function
 
-#Power operation 
-
-elif operation == "^" :
-    print (" ")
-    print ("Your answer is: ", variable_1 ** variable_2)
-    print (" ")
-    print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Binary operation
-
-elif operation == "b":
-    print (" ")
-    print ("You've choosen binary, basic operations allowed only:\n\n1. Addition = +\n2. Subtraction = -\n3. Division = /\n4. Multiplication = *\n5. Power = ^")
-    print(" ")
-    
-#user operation for binary numbers
-
-    binary_operation = input("your binary operation: ")
-    
-#Binary operation [addition]
-
-    if binary_operation == "+":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 + variable_2, "b"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Binary operation [subtraction]
-
-    elif binary_operation == "-":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 - variable_2 , "b"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Binary operation [division]
-
-    elif binary_operation == "/":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 / variable_2, "b"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Binary operation [multiplication]
-
-    elif binary_operation == "*":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 * variable_2, "b"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Binary operation [power]
-
-    elif binary_operation == "^":
-        print (" ")
-        print ("Your answer is: " ,format(variable_1 ** variable_2, "b"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Invalid binary operation
-    else:
-        print (" ")
-        print ("syntax error")
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Percentage operation
-
-elif operation == "%":
-    print (" ")
-    print ("You've choosen percentage, basic operations allowed only:\n\n1. Addition = +\n2. Subtraction = -\n3. Division = /\n4. Multiplication = *\n5. Power = ^")
-    print (" ")
-
-#user operation for percentage
-
-    percentage_operation = input("your percentage operation: ")
-    
-#Percentage operation [addition]
-
-    if percentage_operation == "+":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 + variable_2, "%"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Percentage operation [subtraction]
-
-    elif percentage_operation == "-":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 - variable_2 , "%"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Percentage operation [division]
-
-    elif percentage_operation == "/":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 / variable_2, "%"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Percentage operation [multiplication]
-
-    elif percentage_operation == "*":
-        print (" ")
-        print ("Your answer is: ", format(variable_1 * variable_2, "%"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Percentage operation [power]
-
-    elif percentage_operation == "^":
-        print (" ")
-        print ("Your answer is: " ,format(variable_1 ** variable_2, "%"))
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Invalid percentage operation
-    else:
-        print (" ")
-        print ("syntax error")
-        print (" ")
-        print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
-
-#Inavlid operation
-else:
-    print (" ")
-    print("syntax error")
-    print (" ")
-    print ("Your feedback and suggestion for improvement is highly welcomed\nProudly codded by n1l by73")
+	
+user_input()
